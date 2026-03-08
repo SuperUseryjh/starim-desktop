991d370f41e5f7d875451595aa770d19613f8cbc
Mr_Onion <142563755+SuperUseryjh@users.noreply.github.com>
2026-03-08 21:51:59 +0800

    Chores:删除一些不必要文件

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    minimizeWindow: () => ipcRenderer.send('window-minimize'),
-    maximizeWindow: () => ipcRenderer.send('window-maximize'),
-    closeWindow: () => ipcRenderer.send('window-close'),
-    getPlatform: () => ipcRenderer.invoke('get-platform')
-});
